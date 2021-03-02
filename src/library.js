9b9994af1da9391fb5ff6d7a4bf087abd6d8490e
Harry Maynard <harry.maynard@maximusbc.ca>
2021-03-02 14:47:23 -0500

    Added button to library.

--- src/library.js
+++ src/library.js
@@ -1,7 +1,11 @@
+import Button from './components/Button.vue';
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import Loader from './components/Loader.vue';
 
 export default {
-  Header,
+  Button,
   Footer,
+  Header,
+  Loader
 };
