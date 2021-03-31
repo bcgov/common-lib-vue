e91d3f4bd2c655047fb2475d3f0e1b64f5002374
Cyril Bravo <cyril.bravo@maximusbc.ca>
2021-03-31 16:40:52 -0700

    Added images

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,64 +1,104 @@
 <template>
   <header>
-    <div
-      class="container navbar navbar-dark"
-      aria-label="Header"
-    >
-      <div class="logo-and-title">
-        <a href="http://www2.gov.bc.ca/" tabindex="0">
-          <img
-            alt="BC Logo"
-            classNames="img-fluid d-none d-md-block pointer"
-            :width="181"
-            :src="bcidLogoRev"
-          />
-        </a>
-        <div id="title" class="navbar-brand">
-          {{ heading }}
+    <nav class="container-fluid navbar navbar-expand-lg navbar-dark" aria-label="Header">
+      <div class="container">
+        <div>
+          <div class="logo-container mr-3 d-inline-block align-middle">
+            <a href="https://gov.bc.ca"
+              target="_blank">
+              <img
+                alt="BC Logo"
+                class="print-hide"
+                classNames="img-fluid d-none d-md-block pointer"
+                :width="181"
+                :src="bcidLogoRev"
+              />
+              <img
+                alt="BC Logo"
+                class="print-mainlogo"
+                classNames="img-fluid d-none d-md-block pointer"
+                :width="160"
+                :height="52"
+                :src="bcidMainLogoRev"
+              />
+            </a>
+            
+          </div>
+          <div class="title-container align-middle">
+            <div class="navbar-brand mr-0">
+              {{ title }}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </nav>
   </header>
 </template>
 
 <script>
-export default {
-  name: "Header",
-  props: {
-    heading: {
-      type: String,
-      required: true
+  export default {
+    name: "Header",
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        // bcgovLogo: `/images/bcgov-logo.svg`,
+        // bcgovLogoText: `/images/bcgov-logo-text.svg`,
+        // bcgovLogoPrint: `/images/bcgov-logo-print.png`,
+        bcidSymbol: "/images/bcid-symbol-rev.svg",
+        bcidLogoRev: "/images/bcid-logo-rev-en.svg",
+        bcidMainLogoRev: "/images/bcgov-mainlogo.jpg"
+      }
     }
-  },
-  data() {
-    return {
-      bcidSymbol: "/images/bcid-symbol-rev.svg",
-      bcidLogoRev: "/images/bcid-logo-rev-en.svg"
-    };
   }
-};
 </script>
 
 <style scoped>
-header {
-  background-color: #036;
-  border-bottom: 2px solid #fcba19;
+.navbar {
+  min-height: 65px;
 }
-
 .navbar > .container-fluid {
   width: 95%;
   justify-content: inherit;
 }
-
-#title {
-  white-space: normal;
-}
-
 .pointer {
   cursor: pointer;
 }
+.logo-container {
+  width: 181px;
+  height: 44px;
+}
+.print-mainlogo {
+  display: none;
+}
+@media (max-width: 767.98px) {
+  .logo-container {
+    width: 64px;
+    height: 44px;
+  }
+}
+@media print {
+  .print-hide {
+    display: none !important;
+  }
+  .navbar {
+    display: block;
+  }
+  .print-mainlogo {
+    display: inline-block;
+  }
+  .navbar-dark .navbar-brand {
+    color: #000;
+  }
+}
 
-@media (min-width: 992px) {
-  
+@media (min-width: 768px) {
+    .title-container {
+        display: inline-block !important;
+    }
 }
 </style>
