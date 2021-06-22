380bc2442da0d960a7b2c07cbf056a62358e46da
Harry Maynard <harry.maynard@maximusbc.ca>
2021-06-22 18:19:26 -0400

    Added unit testing framework and unit tests.

---
+++ tests/unit/components/Input.spec.js
@@ -0,0 +1,28 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import Component from '../../../src/components/Input.vue';
+
+const localVue = createLocalVue();
+
+describe('Input.vue', () => {
+  // This is a Shallow Mount as opposed to a regular mount because this test only checks for rendering
+  it('renders', () => {
+    const wrapper = mount(Component, {
+      localVue,
+    });
+    expect(wrapper.element).toBeDefined();
+  });
+
+  it('emits input correctly through built in method', () => {
+    const wrapper = mount(Component, {
+      localVue,
+    });
+    const fakeEvent = {
+      target: {
+        value: "potato"
+      }
+    };
+    wrapper.vm.emitInput(fakeEvent);
+    expect(wrapper.emitted().input).toBeTruthy();
+    expect(wrapper.emitted().input).toEqual([["potato"]]);
+  });
+});
