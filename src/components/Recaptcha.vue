bc78c98c9e7c9fe7b787d7d3e7f41cba17d5c44c
Ian-c-cameron <ian.c.cam@gmail.com>
2021-08-19 15:22:08 -0700

    chore: cleanup unused styles, remove 'this.'

--- src/components/Recaptcha.vue
+++ src/components/Recaptcha.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <vue-recaptcha :sitekey="this.publicKey"
+    <vue-recaptcha :sitekey="publicKey"
                   :loadRecaptchaScript="true"
                   @verify="validate"/>
     <div v-if="errorMessage"
@@ -73,95 +73,4 @@ export default {
 }
 </script>
 
-<style scoped>
-.recaptcha-image-container {
-  display: inline-block;
-  border: solid thin #000;
-}
-.button-container {
-  display: flex;
-  flex-direction: column;
-  width: -webkit-fit-content;
-  width: -moz-fit-content;
-  width: fit-content;
-  justify-content: space-between;
-}
-.recaptcha-group-container {
-  display: flex;
-  flex-direction: column;
-  flex-flow: wrap;
-  justify-content: space-between;
-  max-width: 315px;
-}
-#input-answer {
-  max-width: 150px;
-}
-.svg-defs {
-  display: none;
-}
-.recaptcha-button {
-  background-color: #003366;
-  border: none;
-  border-radius: 4px;
-  color: white;
-  padding: 5px 10px; /* changed from the default 12px 32px */
-  text-align: center;
-  text-decoration: none;
-  font-size: 10px; /* changed from the default 18px */
-  font-family: 'BCSans', 'Noto Sans', Verdana, Arial, sans-serif;
-  font-weight: 700;
-  letter-spacing: 1px;
-  cursor: pointer;
-  box-sizing: border-box;
-
-  /* Added to space icons and text evenly */
-  justify-content: center;
-  align-items: center;
-  display: flex;
-}
-.recaptcha-button:hover {
-  text-decoration: underline;
-  opacity: 0.80;
-}
-.recaptcha-button:focus {
-  box-shadow: 0 0 0 0.2rem rgba(56,89,138,0.25);
-}
-.recaptcha-button:active {
-  opacity: 1;
-}
-.play-audio-button {
-  margin-bottom: 3px;
-}
-.icon-play,
-.icon-loop {
-  display: inline-block;
-  width: 1rem;
-  height: 1rem;
-  stroke-width: 0;
-  stroke: #FFF;
-  fill: #FFF;
-}
-.audio-spinner {
-  display: inline-block;
-  width: 15px;
-  height: 15px;
-  stroke-width: 0;
-  stroke: #FFF;
-  fill: #FFF;
-}
-.button-container > button {
-  height: 28px;
-}
-.button-container > button > span {
-  margin-left: 4px;
-}
-.input-answer {
-  display: inline-block;
-  vertical-align: middle;
-}
-.validation-spinner-container {
-  display: inline-block;
-  margin-left: 15px;
-  vertical-align: middle;
-}
-</style>
+<style scoped></style>
