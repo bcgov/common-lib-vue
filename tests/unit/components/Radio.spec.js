101d8cc87047f510279970b46a11b69fe47118c8
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-26 16:49:54 -0700

    Radio cyValue test

--- tests/unit/components/Radio.spec.js
+++ tests/unit/components/Radio.spec.js
@@ -28,3 +28,74 @@ describe('Radio.vue', () => {
     expect(wrapper.element).toBeDefined();
   });
 });
+
+describe('cyValue()', () => {
+  it('contains cyValue when props contains items with id', () => {
+    const wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        items: [
+          {
+            id: 'yes',
+            label: 'Yes',
+            value: 'Y'
+          },
+          {
+            id: 'no',
+            label: 'No',
+            value: 'N'
+          }
+        ],
+        cypressId: 'potato'
+      }
+    });
+    expect(wrapper.find("[data-cy=potatoyes]").exists()).toBe(true)
+    expect(wrapper.find("[data-cy=potatono]").exists()).toBe(true)
+  });
+  it('contains cyValue when props contains items with label', () => {
+    const wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        items: [
+          {
+            id: null,
+            label: 'Yes',
+            value: 'Y'
+          },
+          {
+            id: null,
+            label: 'No',
+            value: 'N'
+          }
+        ],
+        cypressId: 'potato'
+      }
+    });
+    expect(wrapper.find("[data-cy=potatoYes]").exists()).toBe(true)
+    expect(wrapper.find("[data-cy=potatoNo]").exists()).toBe(true)
+  });
+  it('does not contain cyValue when neither are present', () => {
+    const wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        items: [
+          {
+            id: null,
+            label: null,
+            value: 'Y'
+          },
+          {
+            id: null,
+            label: null,
+            value: 'N'
+          }
+        ],
+        cypressId: 'potato'
+      }
+    });
+    expect(wrapper.find("[data-cy=potatoYes]").exists()).toBe(false)
+    expect(wrapper.find("[data-cy=potatoNo]").exists()).toBe(false)
+    expect(wrapper.find("[data-cy=potatoyes]").exists()).toBe(false)
+    expect(wrapper.find("[data-cy=potatono]").exists()).toBe(false)
+  });
+});
