9bf13cb99d774bd62925ae469d72e12dd8162355
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-26 13:25:32 -0700

    Button unit tests for cyValue

--- tests/unit/components/Button.spec.js
+++ tests/unit/components/Button.spec.js
@@ -7,13 +7,37 @@ import Component from '../../../src/components/Button.vue';
 const localVue = createLocalVue();
 
 describe('Button.vue', () => {
+  let wrapper;
+  beforeEach(() => {});
   it('renders', () => {
-    const wrapper = mount(Component, {
+    wrapper = mount(Component, {
       localVue,
       propsData: {
         label: 'My Button'
       }
     });
     expect(wrapper.element).toBeDefined();
+  });
+  it('returns proper cyValue (text)', () => {
+    wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        label: 'My Button',
+        cypressId: 'potato'
+      }
+    });
+    const result = wrapper.vm.cyValue();
+    expect(result).toEqual("potato")
+  });
+  it('returns proper cyValue (null)', () => {
+    wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        label: 'My Button',
+        cypressId: null
+      }
+    });
+    const result = wrapper.vm.cyValue();
+    expect(result).toBeNull();
   });
 });
