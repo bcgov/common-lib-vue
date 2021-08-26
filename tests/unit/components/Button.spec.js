a79f291841efa17763fd9bc91fd0fbf03d38ac0c
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-26 16:04:49 -0700

    Button cyValue test

--- tests/unit/components/Button.spec.js
+++ tests/unit/components/Button.spec.js
@@ -6,16 +6,26 @@ import Component from '../../../src/components/Button.vue';
 
 const localVue = createLocalVue();
 
-describe.skip('Button.vue', () => {
+describe('Button.vue', () => {
   let wrapper;
   beforeEach(() => {});
   it('renders', () => {
     wrapper = mount(Component, {
       localVue,
       propsData: {
-        label: 'My Button'
+        label: 'My Button',
       }
     });
     expect(wrapper.element).toBeDefined();
+  });
+  it('contains cyValue', () => {
+    wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        label: 'My Button',
+        cypressId: 'potato'
+      }
+    });
+    expect(wrapper.find("button[data-cy=potato").exists()).toBe(true)
   });
 });
