9ba48386e24b1f68e1072abb0feafe55ca2dd36f
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-08-26 16:52:28 -0700

    Select cyValue test

--- tests/unit/components/Select.spec.js
+++ tests/unit/components/Select.spec.js
@@ -14,3 +14,29 @@ describe('Select.vue', () => {
     expect(wrapper.element).toBeDefined();
   });
 });
+
+describe('cyValue()', () => {
+  it('contains cyValue', () => {
+    const wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        cypressId: 'potato',
+        options: [
+          {
+            "value": "A",
+            "label": "Option A"
+          },
+          {
+            "value": "B",
+            "label": "Option B"
+          },
+          {
+            "value": "C",
+            "label": "Option C"
+          }
+        ]
+      }
+    });
+    expect(wrapper.find("[data-cy=potato1]").exists()).toBe(true)
+  });
+});
