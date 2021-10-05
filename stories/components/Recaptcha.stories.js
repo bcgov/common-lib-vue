109ac85a31f7e0b471c9e2ef643596dfead0a7ff
Ian Cameron <ian.cameron@maximuscanada.ca>
2021-10-05 10:15:52 -0700

    chore: rebase merge

--- stories/components/Recaptcha.stories.js
+++ stories/components/Recaptcha.stories.js
@@ -8,13 +8,13 @@ export default {
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { Captcha },
-  template: '<Captcha v-bind="$props" />',
+  components: { Recaptcha },
+  template: '<Recaptcha v-bind="$props" />',
 });
 
 export const Example = Template.bind({});
 Example.args = {
   apiBasePath: '/project/api/recaptcha',
   nonce: '0123456789',
-  publicKey="6LfHcJcbAAAAAJA_kkeR4AXt92hSUpCxb-mKeWkT"
+  publicKey:"6LfHcJcbAAAAAJA_kkeR4AXt92hSUpCxb-mKeWkT"
 };
