a711d374aac41e171c0de5b1772241e368ae89cb
Bryce McMath <bryce.j.mcmath@gmail.com>
2022-02-04 14:22:43 -0800

    Fix header text overflow and story img links

--- stories/components/Header.stories.js
+++ stories/components/Header.stories.js
@@ -15,5 +15,5 @@ const Template = (args, { argTypes }) => ({
 export const Example = Template.bind({});
 Example.args = {
   title: 'My Application Title',
-  imagePath: '/oop/images/'
+  imagePath: '/images/'
 };
