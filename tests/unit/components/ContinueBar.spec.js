0e8acef7a24101336d1b3b1b35c9f7fe9e00334c
Harry Maynard <harry.maynard@maximusbc.ca>
2022-03-11 16:28:01 -0500

    Added `ContinueBar` component unit tests.

--- tests/unit/components/ContinueBar.spec.js
+++ tests/unit/components/ContinueBar.spec.js
@@ -13,6 +13,46 @@ describe('ContinueBar.vue', () => {
     });
     expect(wrapper.element).toBeDefined();
   });
+
+  it('should emit "continue" event when "Continue" button is clicked', async () => {
+    const wrapper = mount(Component, {
+      localVue,
+    });
+    await wrapper.find('button').trigger('click');
+    expect(wrapper.emitted().continue.length).toBe(1);
+  });
+
+  it('should emit "secondary" event when secondary button is clicked', async () => {
+    const wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        hasSecondaryButton: true,
+        secondaryButtonLabel: 'Hello',
+      }
+    });
+    await wrapper.find('button').trigger('click');
+    expect(wrapper.emitted().secondary.length).toBe(1);
+  });
+
+  it('should have "sticky" CSS class when "isSticky" is true', async () => {
+    const wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        isSticky: true,
+      }
+    });
+    expect(wrapper.element.querySelector('.sticky')).toBeDefined();
+  });
+
+  it('should not have "sticky" CSS class when "isSticky" is false', async () => {
+    const wrapper = mount(Component, {
+      localVue,
+      propsData: {
+        isSticky: false,
+      }
+    });
+    expect(wrapper.element.querySelector('.sticky')).toBe(null);
+  });
 });
 
 describe('ContinueBar.vue getCypressValue()', () => {
@@ -26,4 +66,3 @@ describe('ContinueBar.vue getCypressValue()', () => {
     expect(wrapper.find("[data-cy=potato]").exists()).toBe(true)
   });
 });
-
