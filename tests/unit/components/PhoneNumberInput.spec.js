0370b7c17d653f4ccf4cbe2972a273982643da5f
Jonathan Langlois <jonathan.langlois@maximuscanada.ca>
2022-04-27 14:14:53 -0700

    switch to maska for input masking

--- tests/unit/components/PhoneNumberInput.spec.js
+++ tests/unit/components/PhoneNumberInput.spec.js
@@ -1,20 +1,59 @@
-import { mount } from '@vue/test-utils';
-import Component from '../../../src/components/PhoneNumberInput.vue';
+import { mount } from "@vue/test-utils";
+import PhoneNumberInput from "../../../src/components/PhoneNumberInput.vue";
 
-describe('PhoneNumberInput.vue', () => {
-  it('renders', () => {
-    const wrapper = mount(Component);
+describe("PhoneNumberInput.vue", () => {
+  it("renders", () => {
+    const wrapper = mount(PhoneNumberInput);
     expect(wrapper.element).toBeDefined();
   });
 });
 
-describe('PhoneNumberInput getCypressValue()', () => {
-  it('contains cypress Value', () => {
-    const wrapper = mount(Component, {
+describe("PhoneNumberInput getCypressValue()", () => {
+  it("contains cypress Value", () => {
+    const wrapper = mount(PhoneNumberInput, {
       props: {
-        cypressId: 'potato'
-      }
+        cypressId: "potato",
+      },
     });
-    expect(wrapper.find("[data-cy=potato]").exists()).toBe(true)
+    expect(wrapper.find("[data-cy=potato]").exists()).toBe(true);
+  });
+});
+
+describe("PhoneNumberInput event handling", () => {
+
+  it("works correctly with v-model", async () => {
+    const wrapper = mount({
+      data() {
+        return { phone: "2" };
+      },
+      template: '<div> <phone v-model="phone" /></div>',
+      components: { phone: PhoneNumberInput },
+    });
+
+    expect(wrapper.vm.phone).toBe("2");
+    const baseInput = wrapper.find("input");
+    await baseInput.setValue("3");
+
+    // Mask adds a bracket
+    expect(wrapper.vm.phone).toBe("(3");
+  });
+
+  it("applies the expected mask to emitted event", async () => {
+    const wrapper = mount(PhoneNumberInput);
+    const baseInput = wrapper.find("input");
+    const getLastUpdateEmit = () =>
+      wrapper.emitted("update:modelValue").slice(-1)[0][0];
+
+    // First digit must be >= 2
+    await baseInput.setValue("0");
+    expect(getLastUpdateEmit()).toBe("(");
+    await baseInput.setValue("1");
+    expect(getLastUpdateEmit()).toBe("(");
+
+    await baseInput.setValue("211");
+    expect(getLastUpdateEmit()).toBe("(211) ");
+
+    await baseInput.setValue("2111111111");
+    expect(getLastUpdateEmit()).toBe("(211) 111-1111");
   });
 });
