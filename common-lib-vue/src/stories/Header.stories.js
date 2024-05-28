04fd7b04c8f44d37101a7e2d26892a5fc0d32e7a
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-05-28 10:17:18 -0700

    switch header component from storybook default to existing common library component

--- common-lib-vue/src/stories/Header.stories.js
+++ common-lib-vue/src/stories/Header.stories.js
@@ -1,47 +1,21 @@
-import { fn } from '@storybook/test';
-import MyHeader from './Header.vue';
+import Header from '../../src/components/Header.vue'
 
 export default {
-  title: 'Example/Header',
-  component: MyHeader,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  render: (args) => ({
-    // Components used in your story `template` are defined in the `components` object
-    components: {
-      MyHeader,
-    },
-    // The story's `args` need to be mapped into the template through the `setup()` method
-    setup() {
-      // Story args can be spread into the returned object
-      return {
-        ...args,
-      };
-    },
-    // Then, the spread values can be accessed directly in the template
-    template: '<my-header :user="user" />',
-  }),
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
-  },
-  args: {
-    onLogin: fn(),
-    onLogout: fn(),
-    onCreateAccount: fn(),
-  },
-};
+  title: 'Components/Header',
+  component: Header,
+  argTypes: {}
+}
 
-export const LoggedIn = {
-  args: {
-    user: {
-      name: 'Jane Doe',
-    },
+const Template = (args) => ({
+  components: { Header },
+  setup() {
+    return { args }
   },
-};
+  template: '<Header v-bind="args" />'
+})
 
-export const LoggedOut = {
-  args: {
-    user: null,
-  },
-};
+export const Example = Template.bind({})
+Example.args = {
+  title: 'My Application Title',
+  imagePath: '@/stories/assets/'
+}
