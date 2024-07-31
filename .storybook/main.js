460011a50ec3ffcc0412a6f96a554362bc75fbdd
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-07-31 15:38:13 -0700

    disable telemetry, update formatting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,24 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
     // '@chromatic-com/storybook',
     // '@storybook/addon-interactions',
-    'storybook-addon-mock'
+    "storybook-addon-mock",
   ],
 
   framework: {
-    name: '@storybook/vue3-vite',
-    options: {}
+    name: "@storybook/vue3-vite",
+    options: {},
   },
 
-  docs: {}
-}
-export default config
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
+  },
+
+  docs: {},
+};
+export default config;
