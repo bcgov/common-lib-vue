6bea28aab70928ec023b24e68534a5c6b4a06d57
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-08-12 15:58:42 -0700

    migrate from eslintrc.cjs to new flat config format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,35 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+// /* eslint-env node */
+// require("@rushstack/eslint-patch/modern-module-resolution");
 
-module.exports = {
-  root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting',
-    'plugin:storybook/recommended'
-  ],
-  overrides: [
-    {
-      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
-      extends: ['plugin:cypress/recommended']
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-    'no-import-assign': 'off'
-  }
-}
+// module.exports = {
+//   root: true,
+//   plugins: ["compat"],
+//   extends: [
+//     "plugin:compat/recommended",
+//     "plugin:vue/vue3-essential",
+//     // "eslint:recommended",
+//     // "@vue/eslint-config-prettier/skip-formatting",
+//     // "plugin:storybook/recommended",
+//     "plugin:json/recommended-legacy",
+//   ],
+//   overrides: [
+//     {
+//       files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}", "cypress/support/**/*.{js,ts,jsx,tsx}"],
+//       extends: ["plugin:cypress/recommended"],
+//     },
+//     {
+//       files: ["**/*.spec.js", "vitest.config.js"], //disable browser compatability check in unit tests
+//       rules: {
+//         "compat/compat": "off",
+//       },
+//     },
+//   ],
+//   parserOptions: {
+//     ecmaVersion: "latest",
+//     sourceType: "module",
+//   },
+//   ignorePatterns: ["**/coverage/*", "**/dist/*"],
+//   rules: {
+//     "no-unused-vars": "warn",
+//   },
+// };
---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import pluginCypress from "eslint-plugin-cypress/flat";
+import compat from "eslint-plugin-compat";
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  js.configs.recommended, //eslint formatting for basic Javascript syntax
+  pluginCypress.configs.globals, //eslint formatting for Cypress syntax
+  ...pluginVue.configs['flat/recommended'], //eslint formatting for Vue components
+  compat.configs["flat/recommended"], //compatibility with browsers listed in package.json
+  {
+    files: ["**/*.js", "**/*.vue", "**/*.cjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+  {
+    files: ["**/*.spec.js", "vitest.config.js"], //disable browser compatability check in unit tests
+    rules: {
+      "compat/compat": "off",
+    },
+  },
+  {
+    ignores: ["**/coverage/*", "**/dist/*"],
+  }
+];
